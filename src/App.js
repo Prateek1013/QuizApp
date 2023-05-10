bebb5a23b88d450a48d2bf2e8d2bf8ef76b35e96
Prateek1013 <prateek9771196112@gmail.com>
2023-05-11 03:04:16 +0530

    reduced code by map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Quiz from './components/Quiz'
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
-import { QuizData } from './Data/QuizData';
 import { Data } from './Data/Data';
 import "./App.css"
 import Dashboard from './components/Dashboard'
@@ -12,10 +11,11 @@ function App() {
     <div>
       <Routes>
         <Route path='/' element={<Dashboard Data={Data}/>}/>
-        <Route path='/quiz/1' element={<Quiz Data={Data[0].content }/>}/>
-        <Route path='/quiz/2' element={<Quiz Data={Data[1].content }/>}/>
-        <Route path='/quiz/3' element={<Quiz Data={Data[2].content }/>}/>
-        <Route path='/quiz/4' element={<Quiz Data={Data[3].content }/>}/>
+        {
+          Data.map(item => (
+          <Route path={`/quiz/${item.id}`} element={<Quiz Data={item.content }/>}/>
+          ))
+        }
       </Routes>
     </div>
       </Router>
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 const Dashboard = (props) => {
   const {Data}=props;
   const navigate=useNavigate();
-  
+
   const ClickHandler = (QuizNumber)=> {
     navigate(`/quiz/${QuizNumber}`);
   }
@@ -12,28 +12,16 @@ const Dashboard = (props) => {
         <div>
         <h1 >Quiz App</h1>
     <Space direction="horizontal" size={10}>
-    <Card title={`Quiz ${Data[0].id}`} bordered={false} style={{ width: 300 }}>
+    {
+      Data.map(item => (
+        <Card title={`Quiz ${item.id}`} bordered={false} style={{ width: 300 }}>
 
-        <p>{`${Data[0].description}`}</p><br /> <br />
-        <button id='next-button' onClick={() => ClickHandler(Data[0].id)
+        <p>{`${item.description}`}</p><br /> <br />
+        <button id='next-button' onClick={() => ClickHandler(item.id)
              } >Attempt</button>
       </Card>
-      
-      <Card title={`Quiz ${Data[1].id}`} bordered={false} style={{ width: 300 }}>
-
-        <p> {`${Data[1].description}`}</p><br /> <br />
-        <button id='next-button' onClick={()=> ClickHandler(Data[1].id)}>Attempt</button>
-      </Card>
-      <Card title={`Quiz ${Data[2].id}`} bordered={false} style={{ width: 300 }}>
-
-        <p> {`${Data[2].description}`} </p><br /> <br />
-        <button id='next-button' onClick={()=> ClickHandler(Data[2].id)}>Attempt</button>
-      </Card>
-      <Card title={`Quiz ${Data[3].id}`} bordered={false} style={{ width: 240 }}>
-
-        <p> {`${Data[3].description}`}</p><br /> <br />
-        <button id='next-button' onClick={()=>ClickHandler(Data[3].id)}>Attempt</button>
-      </Card>
+      ))
+    }
       </Space>
     </div>   
     );
